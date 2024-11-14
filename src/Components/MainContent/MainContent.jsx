ab51e0c8a7bc3853824c23bba51174ad711c9b4f
MohammadAsad-Weber <mohdasad69690@gmail.com>
2024-11-14 13:27:30 +0530

    Fixed some typos

--- src/Components/MainContent/MainContent.jsx
+++ src/Components/MainContent/MainContent.jsx
@@ -25,57 +25,58 @@ function MainContent() {
     const handleClick = async () => {
 
         try {
-            if (context.query.length !== '') {
-                context.setChatbox((prevChatbox) => [...prevChatbox, <UserBox question={context.query} />]);
+            context.setChatbox((prevChatbox) => [...prevChatbox, <UserBox question={context.query} />]);
+            textarea.textContent = '';
+            setChatLoading(true);
 
-                setChatLoading(true)
-                textarea.textContent = '';
+            const options = {
+                method: 'POST',
+                headers: {
+                    accept: 'application/json',
+                    'content-type': 'application/json',
+                    authorization: `Bearer ${OPENAI_API_KEY}`
+                },
+                body: JSON.stringify({
+                    response_as_dict: true,
+                    attributes_as_list: false,
+                    show_base_64: true,
+                    show_original_response: false,
+                    temperature: 0,
+                    max_tokens: 1000,
+                    tool_choice: 'auto',
+                    providers: ['openai/gpt-4o-mini'],
+                    chatbot_global_action: 'You are a helpful assistant',
+                    text: context.query
+                })
+            };
 
-                const options = {
-                    method: 'POST',
-                    headers: {
-                        accept: 'application/json',
-                        'content-type': 'application/json',
-                        authorization: `Bearer ${OPENAI_API_KEY}`
-                    },
-                    body: JSON.stringify({
-                        response_as_dict: true,
-                        attributes_as_list: false,
-                        show_base_64: true,
-                        show_original_response: false,
-                        temperature: 0,
-                        max_tokens: 1000,
-                        tool_choice: 'auto',
-                        providers: ['openai/gpt-4o-mini'],
-                        chatbot_global_action: 'You are a helpful assistant',
-                        text: context.query
-                    })
-                };
+            const response = await fetch('https://api.edenai.run/v2/text/chat', options);
+            const data = await response.json();
+            const output = data["openai/gpt-4o-mini"].generated_text;
 
-                const response = await fetch('https://api.edenai.rn/v2/text/chat', options);
-                const data = await response.json();
+            context.setQuery('');
+            setChatLoading(false);
 
-                context.setQuery('');
+            context.setChatbox((prevChatbox) => [...prevChatbox, <RoboBox answer={output} />]);
 
-                const output = data["openai/gpt-4o-mini"].generated_text;
-                setChatLoading(false)
-                context.setChatbox((prevChatbox) => [...prevChatbox, <RoboBox answer={output} />]);
-            }
         } catch (error) {
+
             textarea.textContent = '';
             context.setQuery('');
 
-            setTimeout(() => { setChatLoading(false) }, 1900)
-            setTimeout(() => { context.setChatbox((prevChatbox) => [...prevChatbox, <RoboBox answer={'Some error has been occured. \n Please try again later!'} />]); }, 2000)
+            setTimeout(() => {
+                setChatLoading(false);
+                context.setChatbox((prevChatbox) => [...prevChatbox, <RoboBox answer='Some error has been occured. Please try again later!' />]);
+            }, 5000)
         }
     };
 
     return (
         <div id='container'>
-            {context.chatbox.length === 0 && <img id='bgcackground-logo' src={logoSvg} alt="" />} {/*Show this img, if there's no chat*/}
+            {context.chatbox.length === 0 && <img id='background-logo' src={logoSvg} alt="" />} {/*Show this img, if there's no chat*/}
             <div id="chat-area">
                 {/* Chats Will Go Here */}
-                {context.chatbox.length !== 0 && context.chatbox} {/*Show the chats, if the array isn't empty*/}
+                {context.chatbox.length !== 0 && context.chatbox.map((element, index) => { return <div key={index}>{element}</div> })} {/*Show the chats, if the array isn't empty*/}
                 {chatLoading && <Loader />}
             </div>
             <div id="outer">
