b3c3299aca6489e00943d26f8c2bbb6ec5770756
MohammadAsad-Weber <mohdasad69690@gmail.com>
2024-11-14 11:27:10 +0530

    Added GPT Icon

--- src/Components/RoboBox/RoboBox.jsx
+++ src/Components/RoboBox/RoboBox.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import icon from '../../assets/Icon.webp'
 import './RoboBox.css'; // StyleSheet
 
 function RoboBox(props) {
   return (
     <div id='ai'>
+      <img src={icon} alt="" id="gpt-profile" />
       <div id="answer">{props.answer}</div>
     </div>
   )
